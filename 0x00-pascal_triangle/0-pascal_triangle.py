3024be0b7e1f556a06778ea917ad931970512a18
kabandapatrick2580 <sendsome2019@gmail.com>
2024-03-07 17:44:53 +0200

    cnahed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -20,13 +20,3 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
